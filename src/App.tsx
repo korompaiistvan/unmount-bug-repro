b3964e11fe2c062134386161b3ad6070bacf7020
István Korompai <istvan.korompai@apple.com>
2023-03-21 10:59:46 +0000

    feat: add dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Dropdown } from './components/Dropdown'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Dropdown
+        button={(toggleMenu) => (
+          <button
+            onClick={toggleMenu}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Dropdown toggle
+          </button>
+        )}
+      >
+        <>
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
+        </>
+        </Dropdown>
+        
     </div>
   )
 }
---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,91 @@
+import { useRef, useState, ReactElement, ReactNode, useEffect } from "react"
+import './Dropdown.css'
+
+export interface DropdownProps {
+    button: (toggleMenu: () => void) => ReactElement<HTMLButtonElement>
+    children?: ReactNode
+    autoClose?: boolean | 'inside' | 'outside'
+}
+
+export const Dropdown = ({
+    button,
+    children,
+    autoClose = true,
+}: DropdownProps) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+        console.log('toggling')
+    }
+
+    // we add a little timeout to allow children's event listeners to fire before unmounting
+    // FIXME: this is ugly and there is probably a better way
+    const closeMenuWithTimeOut = () => {
+        setTimeout(() => setIsOpen(false), 100)
+    }
+
+    return (
+        <div className="dropdown-wrap">
+            {button(toggleMenu)}
+            {isOpen && (
+                <Menu
+                    onClose={closeMenuWithTimeOut}
+                    autoClose={autoClose}
+                >
+                    {children}
+                </Menu>
+            )}
+        </div>
+    )
+}
+
+interface MenuProps extends Omit<DropdownProps, "button"> {
+    onClose?: () => void
+}
+
+const Menu = ({
+    children,
+    onClose,
+    autoClose,
+}: MenuProps) => {
+    const ref = useRef<HTMLDivElement>(null)
+
+
+    const clickHandler = (event: MouseEvent) => {
+        if (!onClose) return
+        if (autoClose === true) {
+            onClose()
+        }
+
+        if (autoClose === false) {
+            return
+        }
+
+        if (!ref.current) return
+        const isClickInside = ref.current.contains(event.target as Element)
+
+        if (!isClickInside && autoClose === 'outside') {
+            onClose()
+            return
+        }
+
+        if (isClickInside && autoClose === 'inside') {
+            onClose()
+            return
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("mousedown", clickHandler)
+        return () => {
+            window.removeEventListener("mousedown", clickHandler)
+        }
+    }, [])
+
+    return (
+        <nav ref={ref} className='dropdown-menu'>
+            {children}
+        </nav>
+    )
+}
